4ad4b9d4310f259c5c2a59503847b60a2c8cc0b1
ErickElizondo <43624905+ErickElizondo@users.noreply.github.com>
2021-03-31 12:19:31 -0600

    Complete sample

---
+++ js/myJavascript.js
@@ -0,0 +1,17 @@
+
+
+var myObject = [
+    {"nombre": "Erick", "appelido" : "elizondo", "color": "danger"},
+    {"nombre": "aaa", "appelido" : "elizondo", "color": "success"},
+    {"nombre": "bbb", "appelido" : "elizondo", "color": "danger"},
+    {"nombre": "bbb", "appelido" : "elizondo", "color": "info"},
+]
+
+function createCards(){
+    var html = "";
+    for(var i = 0; i < myObject.length; i++){
+        
+
+
+    }
+}
